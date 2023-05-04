b39addf6ada7689606431da7f8fc58e87c4d6c6d
ChanWooKim <chanwoki@student.42seoul.kr>
2023-05-04 16:32:17 +0900

    재채점

--- 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -1,53 +1,23 @@
-def compare_today(compare_day, today):
-    t_year, t_month, t_day = map(int, today.split('.'))
-    if (t_year > compare_day[0]):
-        return (-1)
-    elif (compare_day[0] > t_year):
-        return (1)
-    if (t_month > compare_day[1]):
-        return (-1)
-    elif (compare_day[1] > t_month):
-        return (1)
-    if (t_day > compare_day[2]):
-        return (-1)
-    elif (compare_day[2] > t_day):
-        return (1)
-    return (1)
+def get_today(today):
+    year, month, day = map(int, today.split('.'))
+    return (year * 12 * 28 + month * 28 + day)
 
 def solution(today, terms, privacies):
-    term_dic = {}
+    terms_dic = {}
     answer = []
     idx = 1
+    
     for term in terms:
         key , value = term.split()
-        term_dic[key] = int(value)
-
+        terms_dic[key] = int(value)
+    
+    today = get_today(today)
     for parse in privacies:
         contract, term = parse.split()
         c_year, c_month, c_day = map(int, contract.split('.'))
-        c_day += term_dic[term] * 28
-        while (c_day // 28):
-            c_day -= 28
-            c_month += 1
-        if (c_day == 0):
-            c_day = 28
-            c_month -= 1
-        elif (c_day == 1):
-            c_day = 29
-            c_month -= 1
-        c_day -= 1
-        
-        if (c_month == 0):
-            c_month = 12
-            c_year -= 1
-        while (c_month // 12):
-            c_month -= 12
-            c_year += 1
-        if (c_month == 0):
-            c_month = 12
-            c_year -= 1
-        result = compare_today([c_year, c_month, c_day], today)
-        if (result == -1):
+        c_day += terms_dic[term] * 28
+        compare_day = c_year * 12 * 28 + c_month * 28 + c_day
+        if (compare_day <= today):
             answer.append(idx)
         idx += 1
     return answer
