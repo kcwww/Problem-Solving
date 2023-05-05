ef520ec964466928192c0eff6b07c9a966fc0e56
ChanWooKim <chanwoki@student.42seoul.kr>
2023-05-05 13:18:42 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -0,0 +1,9 @@
+def solution(food):
+    answer = '0'
+    
+    foods = len(food) - 1
+    for i in range(foods, 0, -1):
+        add_food = food[i] // 2
+        answer = str(i) * add_food + answer
+        answer = answer + str(i) * add_food
+    return answer
