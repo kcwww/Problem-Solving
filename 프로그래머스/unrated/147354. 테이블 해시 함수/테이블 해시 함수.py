2ce1e29e1af17f25f629f1d9b29bcc5b72b9d402
ChanWooKim <chanwoki@student.42seoul.kr>
2023-05-15 16:49:43 +0900

    [unrated] Title: 테이블 해시 함수, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/147354. 테이블 해시 함수/테이블 해시 함수.py
@@ -0,0 +1,11 @@
+def solution(data, col, row_begin, row_end):
+    answer = 0
+    data.sort(key=lambda x : [x[col - 1], -x[0]])
+    idx = row_begin
+    for i, num in enumerate(data[row_begin - 1: row_end]):
+        sum_mod = 0
+        for n in num:
+            sum_mod += n % idx
+        idx += 1
+        answer ^= sum_mod
+    return answer
