182116ac2fbdf514a2c0e0eda8a0b384e8175afa
ChanWooKim <chanwoki@student.42seoul.kr>
2023-05-24 18:07:04 +0900

    [level 2] Title: 괄호 변환, Time: 0.87 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/60058. 괄호 변환/괄호 변환.py
@@ -0,0 +1,50 @@
+def check_paren(paren):
+    stack = []
+    if paren[0] == ')':
+        return False
+    for p in paren:
+        if p == ')' and len(stack) > 0:
+            c = stack.pop()
+            if (c != '('):
+                return False
+        elif p == ')':
+            return False
+        else:
+            stack.append(p) 
+    return True
+
+def one_two_three(p):
+    if p == "":
+        return ""
+    l, r = 0, 0
+    u, v = '', ''
+    for i in p:
+        if (l != 0 and l == r):
+            v += i
+            continue
+        if (i == '('):
+            l += 1
+        elif (i == ')'):
+            r += 1
+        u += i
+    if check_paren(u):
+        u += one_two_three(v)
+    else:
+        new_str = '('
+        new_str += one_two_three(v)
+        new_str += ')'
+        u = list(u)
+        u.pop(0)
+        u.pop()
+        for idx, val in enumerate(u):
+            if val == '(':
+                u[idx] = ')'
+            else:
+                u[idx] = '('
+        u = new_str + ''.join(u)
+    return u
+
+def solution(p):
+    if check_paren(p):
+        return p
+    return one_two_three(p)
