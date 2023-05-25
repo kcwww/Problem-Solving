44ad5fcb79157deb24ec896b5c1eb912cde7f6e9
ChanWooKim <chanwoki@student.42seoul.kr>
2023-05-25 13:26:47 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.py
+++ 프로그래머스/lv2/42842. 카펫/카펫.py
@@ -1,20 +1,12 @@
 def solution(brown, yellow):
-    answer = []
-    if yellow % yellow**(1/2) == 0:
-        num = int((brown + yellow)**(1/2))
-        answer = [num, num]
-    else:
-        # brown = 2x + 2(y-2)
-        # yellow = (x - 2) * (y - 2) 
-        # y = num - x
-        # yellow = (x - 2) * (num - 2 - x) 
-        # yellow = -x2 + (num - 2)x + 2x - 2*(num - 2)
-        # x2 -numx + 2*(num - 2) + yellow = 0
-        num = (brown // 2) + 2
-        goal = 2*(num - 2) + yellow
-        for i in range(2, num):
-            if (i * (num - i)) == goal:
-                answer.append(max(i, num - i))
-                answer.append(min(i, num - i))
-                break
-    return answer
+    # brown = 2x + 2(y-2)
+    # yellow = (x - 2) * (y - 2) 
+    # y = num - x
+    # yellow = (x - 2) * (num - 2 - x) 
+    # yellow = -x2 + (num - 2)x + 2x - 2*(num - 2)
+    # x2 -numx + 2*(num - 2) + yellow = 0
+    # ax^2 + bx + c
+    num = ((brown // 2) + 2)
+    b = -num
+    c = 2*(num - 2) + yellow
+    return [int((-b + (b**2 -4*c)**(1/2)) // 2), int((-b - (b**2 -4*c)**(1/2)) // 2)]
