5388f30ab42b4b0620f7d48c4b2c6303d2fdaa04
ChanWooKim <chanwoki@student.42seoul.kr>
2023-05-28 15:03:41 +0900

    [level 2] Title: H-Index, Time: 0.02 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.py
+++ 프로그래머스/lv2/42747. H－Index/H－Index.py
@@ -1,9 +1,10 @@
 def solution(citations):
-    answer = []
-    for h in range(max(citations) + 1):
-        a = list(map(lambda x : x >= h, citations))
-        t = a.count(True)
-        f = a.count(False)
-        if t >= h and f <= h:
-            answer.append(h)
-    return max(answer)
+    cit = sorted(citations)
+    n = len(citations)
+    print(cit)
+    for i in range(n):
+        print(cit[i], i, n, n-i)
+        if cit[i] >= n-i and i < cit[i]:
+            return n-i
+
+    return 0
