fe617ab62826baf45ecfbb99b15ccb9024debec6
ChanWoo Kim <chanwoki@student.42seoul.kr>
2023-12-26 20:22:34 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 17.29 ms, Memory: 41.7 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/README.md
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 택배 배달과 수거하기 - 150369 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150369#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150369) 
 
 ### 성능 요약
 
-메모리: 42.2 MB, 시간: 19.19 ms
+메모리: 41.7 MB, 시간: 17.29 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 2일 20:10:1
+2023년 12월 2일 20:22:32
 
 ### 문제 설명
 
--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.js
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.js
@@ -1,36 +1,71 @@
+
 function findIdx(deliveries, pickups, n) {
-    let i = n -1;
-    while (i >= 0) {
-        if (deliveries[i] !== 0)
-            break;
-        i--;
-    }
-    let j = n - 1;
-    while (j >= 0) {
-        if (pickups[j] !== 0)
-            break;
-        j--;
-    }
-    return [i,j];
+  let i = n - 1;
+  while (i >= 0) {
+    if (deliveries[i] !== 0) break;
+    i--;
+  }
+  let j = n - 1;
+  while (j >= 0) {
+    if (pickups[j] !== 0) break;
+    j--;
+  }
+  return [i, j];
 }
 
 function findNextIdx(deliveries, pickups, i, j) {
-    if (i !== -1) {
-        while (i >= 0) {
-            if (deliveries[i] !== 0)
-                break ;
-            i--;
-        }
+  if (i !== -1) {
+    while (i >= 0) {
+      if (deliveries[i] !== 0) break;
+      i--;
     }
-    
-    if (j !== -1) {
-        while (j >= 0) {
-            if (pickups[j] !== 0)
-                break ;
-            j--;
-        }
+  }
+
+  if (j !== -1) {
+    while (j >= 0) {
+      if (pickups[j] !== 0) break;
+      j--;
     }
-    return [i, j];
+  }
+  return [i, j];
+}
+
+function delivery(deliveries, i, cap) {
+  let volume = 0;
+  while (i >= 0) {
+    volume += deliveries[i];
+
+    if (volume === cap) {
+      deliveries[i] = 0;
+      break;
+    } else if (volume > cap) {
+      deliveries[i] = volume - cap;
+      break;
+    } else {
+      deliveries[i] = 0;
+    }
+    i--;
+  }
+  return i;
+}
+
+function pickup(pickups, j, cap) {
+  let volume = 0;
+  while (j >= 0) {
+    volume += pickups[j];
+
+    if (volume === cap) {
+      pickups[j] = 0;
+      break;
+    } else if (volume > cap) {
+      pickups[j] = volume - cap;
+      break;
+    } else {
+      pickups[j] = 0;
+    }
+    j--;
+  }
+  return j;
 }
 
 function solution(cap, n, deliveries, pickups) {
@@ -39,97 +74,26 @@ function solution(cap, n, deliveries, pickups) {
   // 수거가 0이 아닐때 -> 시작 -> 거기까지는 무조건 가야함 -> 가면서 배달 + 도착후 수거 + 내려오면서 나머지 수거
   // 둘다 0 이 아닐때 -> 시작 -> 거기까지는 무조건 가야함 -> 끝까지 가면서 배달 후 수거 하면서 오기
   // 배달만 있을수도 있고 수거만 있을수도 있음
-  let lastIdx = n - 1;
-  let state = false;
   let distance = 0;
-  let idx = findIdx(deliveries, pickups, n);
-  let i = idx[0];
-  let j = idx[1];
-    
+  let [i, j] = findIdx(deliveries, pickups, n);
+
   while (true) {
-          if (i === -1 && j === -1)
-        break;
+    if (i === -1 && j === -1) break;
     // 맨 끝지점 찾기
     if (i >= 0 || j >= 0) {
-      let d = 0;
-      if (i >= j)
-          d = i;
-      else
-          d = j;
+      const d = (i >= j ? i : j);
       distance += (d + 1) * 2;
     }
 
     // 배달 수거 로직
-
-    let volume = 0;
     if (i >= j) {
-      while (i >= 0) {
-        volume += deliveries[i];
-
-        if (volume === cap) {
-          deliveries[i] = 0;
-          break;
-        } else if (volume > cap) {
-          deliveries[i] = volume - cap;
-          break;
-        } else {
-          deliveries[i] = 0;
-        }
-        i--;
-      }
-
-      volume = 0;
-      while (j >= 0) {
-        volume += pickups[j];
-
-        if (volume === cap) {
-          pickups[j] = 0;
-          break;
-        } else if (volume > cap) {
-          pickups[j] = volume - cap;
-          break;
-        } else {
-          pickups[j] = 0;
-        }
-        j--;
-      }
-
+      i = delivery(deliveries, i, cap);
+      j = pickup(pickups, j, cap);
     } else {
-      while (j >= 0) {
-        volume += pickups[j];
-
-
-        if (volume === cap) {
-          pickups[j] = 0;
-          break;
-        } else if (volume > cap) {
-          pickups[j] = volume - cap;
-          break;
-        } else {
-          pickups[j] = 0;
-        }
-        j--;
-      }
-
-      volume = 0;
-
-      while (i >= 0) {
-        volume += deliveries[i];
-
-        if (volume === cap) {
-          deliveries[i] = 0;
-          break;
-        } else if (volume > cap) {
-          deliveries[i] = volume - cap;
-          break;
-        } else {
-          deliveries[i] = 0;
-        }
-        i--;
-      }
-
+      j = pickup(pickups, j, cap);
+      i = delivery(deliveries, i, cap);
     }
-  [i, j] = findNextIdx(deliveries, pickups, i, j);
+    [i, j] = findNextIdx(deliveries, pickups, i, j);
   }
   return distance;
 }
