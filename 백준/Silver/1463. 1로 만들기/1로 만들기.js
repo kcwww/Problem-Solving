185344d1dcae1e2703b474df1cc9e81bd6c8b444
ChanWoo Kim <chanwoki@student.42seoul.kr>
2024-05-19 17:39:05 +0900

    [Silver III] Title: 1로 만들기, Time: 264 ms, Memory: 42492 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.js
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.js
@@ -1,32 +1,24 @@
-const file = process.platform === "linux" ? "/dev/stdin" : "test.txt";
-const input = require("fs").readFileSync(file).toString().trim().split("\n");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = require("fs")
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n");
 
-const N = +input[0];
+const num = +input[0];
 
-let result = 0;
-
-
-const obj = {
+const dp = {
   1: 0,
   2: 1,
   3: 1,
 };
 
-const dp = (n) => {
-  if (obj[n] !== undefined) {
-    return obj[n];
-  }
+for (let i = 4; i <= num; i++) {
+  dp[i] = dp[i - 1] + 1;
+  if (i % 2 === 0 && i % 3 === 0)
+    dp[i] = Math.min(dp[i / 2] + 1, dp[i / 3] + 1, dp[i]);
+  else if (i % 2 === 0) dp[i] = Math.min(dp[i / 2] + 1, dp[i]);
+  else if (i % 3 === 0) dp[i] = Math.min(dp[i / 3] + 1, dp[i]);
+}
 
-  if (n % 3 === 0 && n % 2 === 0) {
-    obj[n] = Math.min(dp(n / 3), dp(n / 2), dp(n - 1)) + 1;
-  } else if (n % 3 === 0) {
-    obj[n] = Math.min(dp(n / 3), dp(n - 1)) + 1;
-  } else if (n % 2 === 0) {
-    obj[n] = Math.min(dp(n / 2), dp(n - 1)) + 1;
-  } else {
-    obj[n] = dp(n - 1) + 1;
-  }
-  return obj[n];
-};
-
-console.log(dp(N));
+console.log(dp[num]);
